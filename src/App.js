9b20a75029546b85ab8b105ca8040107c843740e
Hye Soo Park <flyjwayur@gmail.com>
2018-11-26 16:27:27 +0200

    Add navigation with routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+
+const Index = () => <h2>Home</h2>;
+const newPost = () => <h2>Write Post</h2>;
+const viewPost = () => <h2>Detail Post</h2>;
+
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/newPost/">new Post</Link>
+          </li>
+          <li>
+            <Link to="/viewPost/">View Post</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Index} />
+        <Route path="/newPost" component={newPost} />
+        <Route path="/viewPost" component={viewPost} />
+        <Route component={() => <h1>The page was not found</h1>}/>
+      </Switch>
+    </div>
+  </Router>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <AppRouter />
       </div>
     );
   }
