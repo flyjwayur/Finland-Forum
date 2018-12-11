c6b63a23282ad0a1f0716b7210b57edfcc05fce0
Hye Soo Park <flyjwayur@gmail.com>
2018-12-11 15:06:14 +0200

    Add try and catch when fetch the data using axios

--- src/store/actions/fetchInfoFromApiAction.js
+++ src/store/actions/fetchInfoFromApiAction.js
@@ -4,10 +4,14 @@ import { FETCH_INFO_FROM_API } from './actionTypes';
 export const fetchInfoFromApi = () => {
   return async(dispatch) => {
     const url = 'https://api.hel.fi/linkedevents/v1/event/';
-    const response = await axios.get(url);
-    dispatch({
-      type : FETCH_INFO_FROM_API,
-      payload : response.data.data
-    })
+    try{
+      const response = await axios.get(url);
+      dispatch({
+        type : FETCH_INFO_FROM_API,
+        payload : response.data.data
+      })
+    } catch(error){
+      console.log(error);
+    }  
   }
 }
