864b0a9b03d66ae25e7529d73834c2e25a531a39
Hye Soo Park <flyjwayur@gmail.com>
2019-03-12 08:31:47 +0200

    Updates

---
+++ precache-manifest.5efbfed38e784bf39ef0e3016dde8b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cfe2476fbd397b1476e",
+    "url": "/Finland-Forum/static/css/main.52cd6069.chunk.css"
+  },
+  {
+    "revision": "4cfe2476fbd397b1476e",
+    "url": "/Finland-Forum/static/js/main.8b239cab.chunk.js"
+  },
+  {
+    "revision": "9cd541f6c98df797d24c",
+    "url": "/Finland-Forum/static/js/runtime~main.0077a889.js"
+  },
+  {
+    "revision": "2b2ee99f43e986606bff",
+    "url": "/Finland-Forum/static/css/2.872e098b.chunk.css"
+  },
+  {
+    "revision": "2b2ee99f43e986606bff",
+    "url": "/Finland-Forum/static/js/2.50518dae.chunk.js"
+  },
+  {
+    "revision": "16178172384d693c399fb4df67854bea",
+    "url": "/Finland-Forum/index.html"
+  }
+];
